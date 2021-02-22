b06d9987d2a84eaefee8639932a11951ec28b7b8
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-02-21 20:53:34 -0500

    adding logic to pull the first transaction involving an asset, parsing the note and idsplaying the image

--- src/Browser.js
+++ src/Browser.js
@@ -2,6 +2,7 @@
 'use strict'
 
 const React = require('react')
+import {Card} from '@blueprintjs/core'
 
 class Browser extends React.Component {
   constructor(props) {
@@ -9,11 +10,12 @@ class Browser extends React.Component {
 
         this.state = {
             token_prefix: "RareAF",
-            tokens:[]
+            tokens:[],
+            metas:[{title:"adsf", artist:"asdfadf", description:"dsaf adsf adsf afdasdfasd asdf adsfasdf asdf asdf"},{},{},{},{}]
         }
         this.getTokens = this.getTokens.bind(this)
 
-        this.getTokens()
+        //this.getTokens()
 
     }
 
@@ -24,8 +26,20 @@ class Browser extends React.Component {
             ledger: 'TestNet',
             path: `/v2/assets?name=RareAF&limit=100`,
         });
-
+        console.log(assets)
         this.setState({tokens:assets.assets})
+
+
+        let notes = []
+        for(let x = 0; x<assets.assets.length; x++){
+            let a = assets.assets[x];
+            const tx = await AlgoSigner.indexer({
+                ledger: 'TestNet',
+                path: `/v2/assets/${a.index}/transactions?max-round=${a['created-at-round']}`
+            });
+            notes.push(JSON.parse(atob(tx.transactions[0].note)))
+        }
+        this.setState({metas:notes})
     }
 
 
@@ -33,12 +47,27 @@ class Browser extends React.Component {
     render() {
         return (
         <div className='container' >
-            <ul>
-            { this.state.tokens.map((asset) => {     
+            
+            { this.state.metas.map((m,i) => {     
                 // Return the element. Also pass key     
-                return (<li >{asset.index}</li> ) 
+                console.log(m)
+                return (
+                    <Card className='content-card' key={i} >
+                        <div className='container'>
+                            <a href={'/raf/'+i}>
+                                <img src='http://via.placeholder.com/550'></img>
+                            </a>
+                        </div>
+                        <div className='container'>
+                            <p><a href={'/raf/'+i}> <b>{m.title}</b> - <i>{m.artist}</i></a></p>
+                        </div>
+                        <div className='container'>
+                            <p>{m.description}</p>
+                        </div>
+                    </Card> 
+                ) 
+                    //    <img src={'http://ipfs.io/ipfs/'+m.file_hash} />
             })}
-            </ul>
         </div>
         )
 
