47778f70abe3a4c0dc88b1ae861667c4a997c941
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-03-14 08:37:49 -0400

    move

--- src/lib/listing.js
+++ src/lib/listing.js
@@ -1,33 +1,54 @@
 import algosdk from 'algosdk'
 
-/*
-    Get teal program template 
-    Get teal program with arguments 
-*/ 
+const platform_settings = {
+    token: {
+        name: "RareAF",
+        unit: "RAF",
+        total: 10000,
+        id: 0
+    },
+    domain: "rare.af",
+}
 
+async function create_platform() {
+    // If platform settings are empty this can get called
+
+    // Create token with name and units
+    // Create Delegated sig to give out this token
+    // save
+}
 
 async function create_listing () {
     const client = algosdk.Algodv2()
+
     // Check user opted into PlatformToken
     // If not, opt into PlatformToken 
 
     // Compile Teal script with appropriate arguments (nft_id, price, creator acct)
     // Set contract acct variable
 
-    // Fund contract acct with algos 
+    // Fund contract acct with 5 algos 
+    // Note contains teal source in case its lost
 
     // Contract acct Opt into NFT
     // Contract acct Opt into Platform Tokens 
 
-    // Fund Contract acct with NFT and Platform Tokens
+    // Send Tx with 
+    //   Delegated Sig PlatformToken xfer 
+    //   Contract Sig NFT xfer
+    //   Contract Sig NFT manager change 
 }
 
 async function destroy_listing(){
     const client = algosdk.Algodv2()
+
     // Send assets and algos back to creator or platform wallet 
     // goal asset send -a 0 -o delist-platform.txn --assetid $PLATFORM_ID -f $CONTRACT_ACCT -t $PLATFORM_ACCT --close-to $PLATFORM_ACCT
     // goal asset send -a 0 -o delist-nft.txn --assetid $NFT_ID -f $CONTRACT_ACCT -t $CREATOR_ACCT --close-to $CREATOR_ACCT 
     // goal clerk send -a 0 -o delist-algo.txn -f $CONTRACT_ACCT -t $CREATOR_ACCT  -F $CONTRACT_NAME  --close-to $CREATOR_ACCT
+
+    // todo: change nft manager back to creator
+
     // goal clerk group -i delist.txn -o delist.txn.grouped
     // goal clerk sign -i delist.txn.grouped -o delist.txn.grouped.signed -p $CONTRACT_NAME
     // goal clerk rawsend -f delist.txn.grouped.signed
