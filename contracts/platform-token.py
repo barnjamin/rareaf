cfbe1abf6307e44681979b3c0ef3de728bdec330
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-03-28 09:48:54 -0400

    maybe working

--- contracts/platform-token.py
+++ contracts/platform-token.py
@@ -19,7 +19,20 @@ def main():
 
     populated_contract  = ScratchVar(TealType.bytes)
 
-    asa_val, contract_val = Btoi(Arg(1)), Arg(2)
+
+    asa_len   = ScratchVar(TealType.uint64)
+    price_len = ScratchVar(TealType.uint64)
+    vars_len  = ScratchVar(TealType.uint64)
+
+    two     = Int(255)
+    three   = Int(65535)
+    four    = Int(16777215)
+    five    = Int(4294967295)
+    six     = Int(1099511627775)
+    seven   = Int(281474976710655)
+    eight   = Int(72057594037927935)
+
+    price_val, asa_val, contract_val = Btoi(Arg(0)), Btoi(Arg(1)), Arg(2)
     intc_start = Int(price[1]) # version, intcblock, number of ints
     bytec_start = Int(acct[1] - (price[2] + asa[2]))
     addr_len = Int(32)
@@ -27,10 +40,29 @@ def main():
     prep_contract = Seq([
         populated_contract.store(contract_val),
 
+        price_len.store(
+            If(price_val<eight, 
+            If(price_val<seven, 
+            If(price_val<six, 
+            If(price_val<five, 
+            If(price_val<four, 
+            If(price_val<three, 
+            If(price_val<two, Int(1),Int(2)), Int(3)), Int(4)), Int(5)), Int(6)), Int(7)), Int(8))),
+
+        asa_len.store(
+            If(asa_val<eight, 
+            If(asa_val<seven, 
+            If(asa_val<six, 
+            If(asa_val<five, 
+            If(asa_val<four, 
+            If(asa_val<three, 
+            If(asa_val<two, Int(1),Int(2)), Int(3)), Int(4)), Int(5)), Int(6)), Int(7)), Int(8))),
+
+        vars_len.store(price_len.load() + asa_len.load()),
         # Cut out the variables, this assumes the 2 vars are adjacent in the assembly
         pre_ints.store(Substring(populated_contract.load(), Int(0), intc_start)), 
-        post_ints.store(Substring(populated_contract.load(), intc_start + Len(Arg(0))+Len(Arg(1)), bytec_start + Len(Arg(0))+Len(Arg(1)))),
-        rest.store(Substring(populated_contract.load(), bytec_start + Len(Arg(0))+Len(Arg(1)) + addr_len, Len(populated_contract.load()))),
+        post_ints.store(Substring(populated_contract.load(), intc_start + vars_len.load(), bytec_start + vars_len.load())),
+        rest.store(Substring(populated_contract.load(), bytec_start + vars_len.load() + addr_len, Len(populated_contract.load()))),
 
         blank_contract.store(Concat(pre_ints.load(), post_ints.load(), rest.load())),
         Int(1)
@@ -87,7 +119,7 @@ def main():
         Gtxn[3].xfer_asset() == platform_token,
         # Is to contract, from  platform 
         Gtxn[3].asset_receiver() == Txn.asset_receiver(),
-        #Gtxn[3].asset_sender() == platform_acct,
+        Gtxn[3].sender() == platform_acct,
         # is for 1 tokens
         Gtxn[3].asset_amount() == Int(1)
     )
