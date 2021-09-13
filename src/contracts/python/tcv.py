025da02ccde82090115808747f0c01743c0dd205
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-09-13 07:50:28 -0400

    adding check for vars and output match

--- src/contracts/python/tcv.py
+++ src/contracts/python/tcv.py
@@ -2,35 +2,50 @@ import json
 import uvarint
 import sys
 
-#goal clerk compile ../listing.tmpl.teal
-#python tcv.py ../listing.tmpl.teal
-#goal clerk compile -D listing.tmpl.teal.populated
+# goal clerk compile ../listing.tmpl.teal
+# python tcv.py ../listing.tmpl.teal
+# goal clerk compile -D listing.tmpl.teal.populated
+# diff ../listing.tmpl.teal.tok ../listing.tmpl.teal.blanked
 
+
+# blank_contract takes a path to a template contract and writes out a 
+# version with the populated template variables replaced with 0s
+# it assumes the populated version will be in the same dir with .populated suffix
+# it also assumes the .tok.deets.json will be there with the template variable details
 def blank_contract(tc):
     contract = list(get_contract(tc+".populated"))
     details = get_details(tc+".tok.deets.json")
 
     # Make sure they're sorted into the order they appear in 
-    # the contract or the `shift` will be wrong
+    # the contract or the positions will get weird
     labels = dict(sorted(details['template_labels'].items(), key=lambda item: item[1]['position']))
 
+    found = {}
     for k,v in labels.items():
         pos = v['position']
         if v['bytes']:
             val, l = uvarint.decode(contract[pos:])
             total = l + val
-            print("{} set to {}".format(k,  contract[pos:pos+total]))
+            found[k] = bytearray(contract[pos+l:pos+total]).hex()
             contract[pos:pos+total] = [0]
         else:
             val, l = uvarint.decode(contract[pos:])
             total = l  
-            print("{} set to {}".format(k,  val))
+            found[k] = val  
             contract[pos:pos+total] = [0]
 
     with open(tc+".blanked", "wb") as blanked_contract:
         blanked_contract.write(bytearray(contract))
         print("Wrote blanked contract to {}".format(tc+".blanked"))
 
+    return found
+
+
+# populate_contract takes a path to a template contract and set of named vars
+# and inserts them into the contract bytecode, taking care to set appropriate length
+# fields and adjusting the positions for the difference in lengths accrued
+# it assumes the compiled bytecode is in the same directory with a `.tok` suffix
+# it also assumes the template variable details are present in the same dir with `.tok.deets.json` suffix
 def populate_contract(tc, tvars):
     contract = list(get_contract(tc+".tok"))
     details = get_details(tc+".tok.deets.json")
@@ -76,6 +91,16 @@ def get_details(fname):
     return details
 
 
+def check_match(fname):
+    original = None
+    blanked = None
+    with open(fname+".tok", "rb") as f:
+        original = f.read()
+    with open(fname+".blanked", "rb") as f:
+        blanked = f.read()
+
+    return original == blanked
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
@@ -84,7 +109,7 @@ if __name__ == "__main__":
 
     print("populating {}".format(sys.argv[1]))
 
-    populate_contract(sys.argv[1], {
+    vars = {
         "TMPL_APP_ID": 1231123,
         "TMPL_ASSET_ID": "0000000000000000",
         "TMPL_CREATOR_ADDR": "deadbeef",
@@ -92,8 +117,17 @@ if __name__ == "__main__":
         "TMPL_OWNER_ADDR": "deadbeef",
         "TMPL_PRICE_ID": 5,
         "TMPL_SEED_AMT": 10000 
-    })
+    }
 
-    blank_contract(sys.argv[1])
+    populate_contract(sys.argv[1], vars)
+
+    found_vars = blank_contract(sys.argv[1])
+
+    for k,v in vars.items():
+        print("{} match? {}".format(k, v == found_vars[k]))
+
+
+    print("Blanked Match original?: {}".format(check_match(sys.argv[1])))
+
 
     # check to make sure the argv[1].tok == argv[1].blanked
